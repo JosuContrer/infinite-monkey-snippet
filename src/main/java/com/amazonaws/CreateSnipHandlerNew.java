117df3db2df65ce38b63ead0d8384e6a58db85fb
wscampbell <wscampbell@wpi.edu>
2020-11-12 15:42:08 -0500

    updated createhandler to match format of other handlers

--- src/main/java/com/amazonaws/CreateSnipHandlerNew.java
+++ src/main/java/com/amazonaws/CreateSnipHandlerNew.java
@@ -1,85 +1,53 @@
 package com.amazonaws;
-//package com.amazonaws.lambda.demo;
-//
-//import java.io.PrintWriter;
-//import java.io.StringWriter;
-//
-//import com.amazonaws.labda.demo.http.CreateRequest;
-//import com.amazonaws.labda.demo.http.CreateResponse;
-//import com.amazonaws.lambda.demo.db.SnippetDAO;
-//import com.amazonaws.lambda.demo.model.Result;
-//import com.amazonaws.lambda.demo.model.Snippet;
-//import com.amazonaws.services.lambda.runtime.Context;
-//import com.amazonaws.services.lambda.runtime.LambdaLogger;
-//import com.amazonaws.services.lambda.runtime.RequestHandler;
-//import com.amazonaws.util.json.Jackson;
-//import com.fasterxml.jackson.databind.JsonNode;
-//
-//public class CreateSnipHandlerNew implements RequestHandler<CreateRequest, CreateResponse> {
-//	
-//	LambdaLogger logger;
-//	
-//	
-//	@Override
-//	public CreateResponse handleRequest(CreateRequest input, Context context) {
-//		logger = context.getLogger();
-//		logger.log("Loading Java Lambda of CreateSnippet");
-//		logger.log(input.toString());
-//		
-//		boolean fail = false;
-//		String failMessage = "";
-//		String snippetId = null;
-//		
-//		// Parse to get only the body form the API Gateway
-//		JsonNode node = Jackson.fromJsonString(incoming.toString(), JsonNode.class);
-//		if (node.has("body")) {
-//			node = Jackson.fromJsonString(node.get("body").asText(), JsonNode.class);
-//		}
-//			
-//		// Processes the JSON password and creation of snippet 
-//		int statusCode = 200;
-//		
-//		// Processes the password TODO: check feasible password and no special characters
-//		String passParam = node.get("password").asText();
-//		
-//		try {
-//			
-//			SnippetDAO snipdao = new SnippetDAO();
-//			Snippet snip = null;
-//			
-//			if (passParam.length() < 32) {
-//				snip = new Snippet(snipdao.getAllSnippets(), passParam);
-//			}
-//			else {
-//				statusCode = 400;
-//			}
-//			
-//			// TODO - probably breaks code
-//			snipdao.addSnippet(snip);
-//			
-//			// Processes RESPONSE
-//			PrintWriter pw = new PrintWriter(output);
-//			
-//			// Create JSON response
-//			String response = Result.ResultJSON(statusCode, snip.getID());
-//			
-//			// Send Response
-//			//   Codes API: 200 (Snippet created), 400 (Invalid snippet)
-//			pw.print(response);
-//			pw.close();
-//		}
-//		catch (Exception e) {
-//			PrintWriter pw = new PrintWriter(output);
-//			
-//			StringWriter errSW = new StringWriter();
-//			PrintWriter errPW = new PrintWriter(errSW);
-//			e.printStackTrace(errPW);
-//			
-//			String response = Result.ResultJSON(statusCode, Result.ErrorJSON(e.getMessage(), errSW.toString()));
-//			
-//			pw.print(response);
-//			pw.close();
-//		}
-//	}
-//}
-//
+
+import com.amazonaws.db.SnippetDAO;
+import com.amazonaws.http.CreateRequest;
+import com.amazonaws.http.CreateResponse;
+import com.amazonaws.model.Snippet;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class CreateSnipHandlerNew implements RequestHandler<CreateRequest, CreateResponse>{
+
+	LambdaLogger logger;
+
+	@Override
+	public CreateResponse handleRequest(CreateRequest input, Context context) {
+
+		logger = context.getLogger();
+
+		if (context != null) {
+			context.getLogger();
+		}
+
+		logger.log("-> Snippet Create Snippet Request: " + input.toString());
+
+        CreateResponse response;
+        int statusCode = 200;
+
+        String pw = input.getPass();
+
+		try{
+
+            SnippetDAO snipdao = new SnippetDAO();
+			Snippet snip = null;
+
+            if (pw.length() < 32) {
+                snip = new Snippet(snipdao.getAllSnippets(), pw);
+            } else {
+                statusCode = 400;
+            }
+
+            snipdao.addSnippet(snip);
+
+            response = new CreateResponse(snip.getID(), statusCode);
+
+		} catch(Exception e) {
+            logger.log(e.getMessage());
+            return new CreateResponse("0", 404);
+        }
+
+        return response;
+	}
+}
