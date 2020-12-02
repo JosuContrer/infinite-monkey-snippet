a65b869f8387a0a2cb4838c6d0485a076b6aabea
JosuContrer <jdcontrerasalbuj@wpi.edu>
2020-12-02 13:26:13 -0500

    Fixed css and made it responsive for snippet page

--- app/src/components/Snippet.js
+++ app/src/components/Snippet.js
@@ -6,7 +6,6 @@ import AceEditor from "react-ace";
 import NavBar from './NavBar';
 import LangDropdown from './LangDropDown';
 import CommentList from './CommentList';
-import { callLambda, sanitizeText } from "./Utilities";
 
 import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/theme-monokai";
@@ -43,6 +42,7 @@ class Snippet extends Component {
 
         this.infoChanged = this.infoChanged.bind(this);
         this.textChanged = this.textChanged.bind(this);
+        this.onSelectionChanged = this.onSelectionChanged.bind(this);
 
         this.textSubmit = this.textSubmit.bind(this);
         this.infoSubmit = this.infoSubmit.bind(this);
@@ -54,14 +54,14 @@ class Snippet extends Component {
     componentDidMount() {
 
         // GET SNIPPET DATA
-        callLambda(this, this.state.url, "GET")
+        Snippet.callLambda(this, this.state.url, "GET")
             .then(response => {
                 let timestampNum = response["timestamp"];
                 let unixDate = new Date(timestampNum);
 
                 this.setState({
                     password: response["password"],
-                    languageText: response["language"].toString(),
+                    languageText: response["language"],
                     timestampText: unixDate.toLocaleString(),
                     info: response["info"],
                     text: response["text"],
@@ -70,6 +70,64 @@ class Snippet extends Component {
             .catch(error => {
                 console.log(error);
             });
+    }
+
+    static sanitizeText(text) {
+        const backspace = String.fromCharCode(8);
+        const formfeed = String.fromCharCode(12);
+        const newline = String.fromCharCode(10);
+        const carriage = String.fromCharCode(13);
+        const tab = String.fromCharCode(9);
+        const quote = String.fromCharCode(34);
+        const backslash = String.fromCharCode(92);
+
+        let cursedArray = [backslash, backspace, formfeed, newline, carriage, tab, quote];
+        let blessedArray = ['\\', '\\b', '\\f', '\\n', '\\r', '\\t', '\\"']
+
+        let snipText = text.replace(/[\x5c\x08\x0c\x0a\x0d\x09\x22]/g, function(x) {
+            let i = cursedArray.indexOf(x);
+
+            return blessedArray[i];
+        });
+
+        return snipText;
+    }
+
+    static callLambda(extThis, url, type, data=null, password=null) {
+        console.log("Request " + type + " @ " + url);
+
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open(type, url, true);
+
+            if (password !== null) {
+                data["password"] = password;
+            }
+
+            xhr.onload = () => {
+                if(xhr.status >= 200 && xhr.status < 300){
+                    console.log("XHR: " + xhr.responseText);
+
+                    resolve(JSON.parse(xhr.responseText));
+                }
+                else {
+                    console.log("    Request Failed: " + xhr.status);
+
+                    reject(xhr.statusText);
+                }
+            };
+
+            xhr.onerror = () => reject(xhr.statusText);
+
+            if (data !== null) {
+                xhr.setRequestHeader("Content-Type", "application/json");
+
+                xhr.send(JSON.stringify(data));
+            }
+            else {
+                xhr.send();
+            }
+        });
     }
 
     editInfo(event) {
@@ -129,22 +187,22 @@ class Snippet extends Component {
     }
 
     infoSubmit(event) {
-        let infoText = sanitizeText(this.state.info);
+        let infoText = Snippet.sanitizeText(this.state.info);
 
         let data = {};
         data["info"] = infoText;
-        callLambda(this, this.state.url + "/updateInfo", "POST",  data, this.state.inputtedPass)
+        Snippet.callLambda(this, this.state.url + "/updateInfo", "POST",  data, this.state.inputtedPass)
             .catch(error => {
                 console.log(error);
             });
     }
 
     textSubmit(event) {
-        let textText = sanitizeText(this.state.text);
+        let textText = Snippet.sanitizeText(this.state.text);
 
         let data = {};
         data["text"] = textText;
-        callLambda(this, this.state.url + "/updateText", "POST", data)
+        Snippet.callLambda(this, this.state.url + "/updateText", "POST", data)
             .catch(error => {
                 console.log(error);
             });
@@ -152,7 +210,7 @@ class Snippet extends Component {
 
     deleteSnippet(event) {
         let delURL = this.state.url + "/deleteSnippet"
-        callLambda(this, delURL, "POST", {}, this.state.inputtedPass)
+        Snippet.callLambda(this, delURL, "POST", {}, this.state.inputtedPass)
             .then(response => {
                 if (response !== null) {
                     window.open("/", "_self");
@@ -163,6 +221,17 @@ class Snippet extends Component {
             });
 
         event.preventDefault();
+    }
+
+    onSelectionChanged(editor){
+        let startRow = editor.anchor.row + 1;
+        let endRow = editor.cursor.row + 1;
+
+        this.setState({
+            startSelection: startRow,
+            endSelection: endRow,
+        });
+        console.log("Cursor Selection: " + this.state.startSelection + ", " + this.state.endSelection);
     }
 
     // Set language from dropdown select
@@ -197,7 +266,7 @@ class Snippet extends Component {
                         </div>
                         <div id="langDiv" className="rightCol">
                             <div id="accessory">
-                                <h5 id="languageText">Language: {this.state.languageText}</h5>
+                                <h5 id="languageText" className="languageTitle">Language: {this.state.languageText}</h5>
                            </div>
                             <button id="infoButton" type="button" onClick={this.editInfo}>Edit Info</button>
                         </div>
@@ -208,7 +277,7 @@ class Snippet extends Component {
                                 <LangDropdown func={this.setLanguage} />
                             </div>
                             <div className="equalCol">
-                                <h3 id="snippetId">Snippet ID: {this.state.snippetID}</h3>
+                                <h3 className="idTitle" id="snippetId">Snippet ID: {this.state.snippetID}</h3>
                             </div>
                             <div className="equalCol">
                                 <h3 className="timeTitle" id="timestampText">Created: {this.state.timestampText}</h3>
@@ -218,15 +287,15 @@ class Snippet extends Component {
                     <div className="snippetsection">
                         <div className="break"></div>
                         <div id="textDiv" className="leftCol">
-                            <h5 className="commentsTitle">Text</h5>
-                            <br/>
                             <AceEditor
+                                ref="mainEditor"
                                 placeholder=""
                                 mode={this.state.languageText}
                                 width={"100%"}
                                 theme={"monokai"}
                                 height={"600px"}
                                 onChange={this.textChanged}
+                                onSelectionChange={this.onSelectionChanged}
                                 fontSize={14}
                                 showPrintMargin={true}
                                 showGutter={true}
@@ -234,17 +303,17 @@ class Snippet extends Component {
                                 value={this.state.text}
                                 style={reactStyle.aceStyle}
                                 setOptions={{
-                                    // enableBasicAutocompletion: false,
-                                    // enableLiveAutocompletion: false,
-                                    // enableSnippets: false,
+                                    enableBasicAutocompletion: false,
+                                    enableLiveAutocompletion: false,
+                                    enableSnippets: false,
                                     showLineNumbers: true,
                                     tabSize: 2,
                                 }}/>
                         </div>
                         <div id="commentDiv" className="rightCol">
-                            <h5 className="commentsTitle">Comments</h5>
+                            <h5 class="commentsTitle">Comments</h5>
                             <br/>
-                            <CommentList snipID={this.state.snippetID}/>
+                            <CommentList snipID={this.state.snippetID} startSel={this.state.startSelection} endSel={this.state.endSelection}/>
                             <button type="button" onClick={this.textSubmit}>Save Text</button>
                         </div>
                     </div>
