894b7823ae675860f24fab7cea376fca9ccaaf1a
JosuContrer <jdcontrerasalbuj@wpi.edu>
2020-12-06 17:13:02 -0500

    Comment Delete Handler Done

--- src/main/java/com/amazonaws/http/comment/DeleteCommentRequest.java
+++ src/main/java/com/amazonaws/http/comment/DeleteCommentRequest.java
@@ -5,5 +5,28 @@ import java.io.Serializable;
 public class DeleteCommentRequest implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	public String snippetId;
+	public String commentId;
+	public String password;
+	
+	public String getSnippetId() {
+		return snippetId;
+	}
+	public void setSnippetId(String snippetId) {
+		this.snippetId = snippetId;
+	}
+	public String getCommentId() {
+		return commentId;
+	}
+	public void setCommentId(String commentId) {
+		this.commentId = commentId;
+	}
+	public String getPassword() {
+		return password;
+	}
 
+	public String toString() {
+		return "DeleteCommentRequest(" + this.snippetId + ", " + this.commentId + ")";
+	}
 }
--- src/main/java/com/amazonaws/http/comment/DeleteCommentResponse.java
+++ src/main/java/com/amazonaws/http/comment/DeleteCommentResponse.java
@@ -1,5 +1,30 @@
 package com.amazonaws.http.comment;
 
 public class DeleteCommentResponse {
+	
+	private int statusCode;
+	private String statusMessage;
+	
+	public DeleteCommentResponse(int sc, String sm) {
+		this.statusCode = sc;
+		this.statusMessage = sm;
+	}
+	
+	public int getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+	public String getStatusMessage() {
+		return statusMessage;
+	}
+	public void setStatusMessage(String statusMessage) {
+		this.statusMessage = statusMessage;
+	}
+
+	public String toString() {
+		return "DeleteComentResponse(" + this.statusCode + ", " + this.statusMessage + ")";
+	}
 
 }
